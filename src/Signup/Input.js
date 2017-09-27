c4ed3402817d959d6bc273304f9c56138cc654a9
Kristopher Borel <k.borel3@gmail.com>
2017-09-27 18:15:03 +1000

    Add form presentational components

---
+++ src/Signup/Input.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export default styled.input`
+  width: 400px;
+  border-radius: 3px;
+  border-color: rgb(230, 236, 240);
+  border-style: solid;
+  border-width: 1px;
+  margin-bottom: 25px;
+  padding: 15px;
+
+  font-weight: bold;
+  font-size: 18px;
+  &::placeholder {
+    color: lightgray;
+  }
+
+  &:focus {
+    border-color: lightblue;
+  } 
+`;
--- src/Signup/SignupForm.js
+++ src/Signup/SignupForm.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
+import Input from './Input';
+import Button from './Button';
 
 class SignupForm extends Component {
   render() {
     return (
-      <div>
-        
-      </div>
+      <form style={{display: 'block'}}>
+        <Input type='text' placeholder='Full name'/>
+        <Input type='text' placeholder='Email'/>
+        <Input type='password' placeholder='Password'/>
+        <Button type='submit' value='Sign Up'/>
+      </form>
     );
   }
 }
