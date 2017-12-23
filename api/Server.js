f059dea0a5a24c127e424a8fc5f25b26518044ba
Kristopher Borel <k.borel3@gmail.com>
2017-12-23 16:53:35 +1100

    Remove console log from server

--- api/Server.js
+++ api/Server.js
@@ -3,6 +3,4 @@ const app = require('./App');
 require('dotenv').config();
 
 app.set('port', process.env.PORT || 7777);
-const server = app.listen(app.get('port'), () => {
-  console.log(`Application running on PORT ${server.address().port}`);
-});
+app.listen(app.get('port'));
