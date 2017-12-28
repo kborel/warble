e015be5ebf27c2af09e4dbec0e7c4c5728de0489
Kristopher Borel <k.borel3@gmail.com>
2017-12-28 17:38:03 +1100

    Add body-parser

--- api/app.js
+++ api/app.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const routes = require('./routes/index');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/', routes);
 
