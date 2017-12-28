b713baf5b753ecb5968cae4fdf74fe1e11f34947
Kristopher Borel <k.borel3@gmail.com>
2017-12-28 19:21:06 +1100

    Add redux store

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
+import store from './store';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+if(module.hot) {
+  module.hot.accept('./App', () => {
+    ReactDOM.render(
+      <Provider store={store}>
+        <App />
+      </Provider>
+      , document.getElementById('root'));
+  })
+}
+
 registerServiceWorker();
