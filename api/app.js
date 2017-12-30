35f3f2e64748b3483bd5c354be4842b1749e7459
Kristopher Borel <k.borel3@gmail.com>
2017-12-30 13:20:00 +1100

    Add cors to server

--- api/app.js
+++ api/app.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const routes = require('./routes/index');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const errorHandlers = require('./handlers/errorHandlers');
 
 const app = express();
+
+app.use(cors());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
