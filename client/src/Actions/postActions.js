c7e0ce3b52b79098dfa793ae4e70ba74462ef580
Kristopher Borel <k.borel3@gmail.com>
2017-12-31 21:27:52 +1100

    Add submitted post to redux state

--- client/src/Actions/postActions.js
+++ client/src/Actions/postActions.js
@@ -1,16 +1,16 @@
 import axios from 'axios';
 import config from '../config.json';
 
-export const SUBMIT_POST = 'SEND_POST';
+export const POST_SUBMITTED = 'POST_SUBMITTED';
 
-export const submitPost = post =>
+export const submitPost = text =>
   async (dispatch) => {
     try {
       const { data } = await axios.post(`${config.apiRoot}/posts`, {
-        post,
+        text,
       });
       dispatch({
-        type: SUBMIT_POST,
+        type: POST_SUBMITTED,
         payload: data,
       });
     } catch (error) {
--- client/src/Reducers/postsReducer.js
+++ client/src/Reducers/postsReducer.js
@@ -1,5 +1,14 @@
+import { POST_SUBMITTED } from '../Actions/postActions';
+
 export default function posts(state = {}, action) {
   switch (action.type) {
+    case POST_SUBMITTED:
+      return {
+        ...state,
+        [action.payload.id]: {
+          ...action.payload,
+        },
+      };
     default:
       return state;
   }
